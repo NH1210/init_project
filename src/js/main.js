8d9b3d940377991ec970f7513a2a9bef24cafa2f
Nh's <macbook@MacBooks-MacBook-Pro.local>
2019-06-28 09:59:13 +0700

    first blood

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,3 @@
-import { header } from './header'
-import { footer } from './footer'
-
 window.addEventListener('DOMContentLoaded', function () {
-  header()
-  footer()
-
-  fetch('data.json')
-  .then(response => response.json())
-  .then(json => {
-    console.log(json.branches)
-  });
+  console.log('done')
 })
